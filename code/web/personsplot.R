56370aec82685d8006d210d9f60bf02d48f9969f
kishorekarthick23 <kishorekarthickgs23@gmail.com>
2023-03-24 11:12:15 -0400

    Updating the plot

--- code/web/personsplot.R
+++ code/web/personsplot.R
@@ -3,13 +3,15 @@ library(tidyverse)
 library(readxl)
 
 
-data <- read_excel("~/fairfax-snap-app/code/web/img/Fairfax_SNAP_2005_2023_dataset.xlsx")
-data <- data.frame(data)
+
+data1 <- read_excel("~/fairfax-snap-app/code/web/img/fairfax_SNAP_with_persons_avg.xlsx")
+data1 <- data.frame(data1)
+
 
 
 # plot for number of persons
 
-plot1 <- data %>%
+plotp1 <- data1 %>%
   plot_ly(
     x = ~Month,
     y = ~PERSONS.TOTAL,
@@ -38,4 +40,32 @@ plot1 <- data %>%
   )
 
 
-plot1
+# plot for avg amount by persons receiving SNAP
+plotp2 <- data1 %>%
+  plot_ly(x = ~Month,
+          y = ~Snap_per_person, 
+          type = "scatter", mode = "lines",
+          name = "Avg SNAP amount received by persons",
+          hovertemplate = paste("<b>Month:</b> %{x}<br>",
+                                "<b>Average SNAP Amount:</b> %{y:.2f}<br>", "<extra></extra>")) %>%
+  layout(
+    yaxis = list(
+      title = "Snap Amount ",
+      tickformat = ".0f"
+    ),
+    xaxis = list(
+      rangeslider = list(
+        autorange = TRUE,
+        range = c(as.Date("2005-01-01"), as.Date("2023-12-01")),
+        thickness = 0.1,
+        bgcolor = "#F5F6F9"
+      )
+    ),
+    title = " "
+  )
+
+subplot(
+  plotp1,plotp2, margin = 0.1) %>%  layout(width = 1000) %>% layout(legend = list(orientation = "h", 
+                                                                                  xanchor = "center",
+                                                                                  x = 0.5,
+                                                                                  y = -0.2) ) 
